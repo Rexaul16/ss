efbb54d31b2074e0e547a4704126276766104706
Rexaul16 <mendoza.saul16@gmail.com>
2015-09-18 14:51:41 -0700

    Update main.cxx

--- main.cxx
+++ main.cxx
@@ -1,41 +1,27 @@
-include <iostream>
+#include <iostream>
 #include "fraction.h"
 
 using namespace std;
 using bein_cs202::Fraction;
 
-void crossMultiply(Fraction a, Fraction b, Fraction &result)
-{
-  long m,x;
-
-  result.set_numerator(a.get_numerator() * b.get_denominator() + a.get_denominator() * b.get_numerator());
-
-  result.set_denominator((a.get_denominator() * b.get_denominator()));
-
-}
-
 void initializeFraction(Fraction &f, unsigned long numerator, unsigned long denominator)
 {
   long m,x;
-
   f.set_numerator(numerator);
   f.set_denominator(denominator);
 }
-
 void findNthHarmonic(unsigned char n, Fraction &result)
 {
-  Fraction a, b;
-  initializeFraction(a,1,1);
-  initializeFraction(b,1,2);
-  for(int i = 2; i<= n; i++)
+  Fraction a, b,c;
+  initializeFraction(a,1,n);
+  for(int i = n-1; i> 0; i--)
     {
-      b.set_denominator(i);
-      crossMultiply(a,b,a);
+      initializeFraction(b,1,i);
+      a = a + b;
     }
   result.set_denominator(a.get_denominator());
   result.set_numerator(a.get_numerator());
 }
-
 int main()
 {
   int n;
@@ -45,10 +31,10 @@ int main()
     {
       findNthHarmonic(n,harmonic);
       harmonic.reduce();
-      harmonic.print();
-      cout << endl;
+      cout << harmonic.get_numerator() << "/" << harmonic.get_denominator() << endl;
+     cout << endl;
     }
   else if(n>=20)
     cout << "The number you input is too large to calculate" << endl;
-      return 0;
+  return 0;
 }
